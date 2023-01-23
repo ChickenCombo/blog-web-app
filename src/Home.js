2cebb8b3a6767b7b0043f838666f30c3b5dbcdb2
Lester Cuadra <lester.cuadra24@gmail.com>
2023-01-23 18:32:06 +0800

    initial commit

---
+++ src/Home.js
@@ -0,0 +1,17 @@
+import BlogList from './BlogList';
+import useFetch from './useFetch';
+
+const Home = () => {
+  const { data: blogs, isPending, error } = useFetch('http://localhost:8000/blogs')
+
+  return (
+    <div className="home">
+      <h2>All Blogs</h2>
+      { error && <div>{error}</div> }
+      { isPending && <div>Loading...</div> }
+      <BlogList blogs={blogs} />
+    </div>
+  );
+}
+
+export default Home;
